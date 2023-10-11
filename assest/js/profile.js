10aa82a1f820629190fce7cb971d4c16a657e6e7
Ranajawabry <rana.a.jawabry@gmail.com>
2023-10-11 18:17:45 +0300

    add user information

--- assest/js/profile.js
+++ assest/js/profile.js
@@ -15,19 +15,25 @@ const getUserDetails = async()=>{
 getUserDetails()
 
 const display=(data)=>{
+    const title = document.querySelector('.title');
+    title.innerHTML= `${data.firstName} Portfolio`;
     const userData = document.querySelector('.userData')
     const result =`
     <div class="row">
-    <div class="img col-2">
+    <div class="img col-3">
         <img src=${data.image} class='w-100' alt="">
     </div>
     <div class="col-9 info">
-        <p class="text-capitalize fs-5 fw-bold">full name: ${data.firstName}+${data.maidenName} </p>
-        <p class="text-capitalize fs-5 fw-bold">address:   ${data.address.address}-${data.address.city} </p>
-        <p class="text-capitalize fs-5 fw-bold">birth date:   ${data.birthDate}</p>
-        <p class="text-capitalize fs-5 fw-bold">phone:  ${data.phone} </p>
-        <p class="text-capitalize fs-5 fw-bold">email:  ${data.email} </p>
-        <p class="text-capitalize fs-5 fw-bold">univercity: ${data.university}</p>
+        <p class="text-capitalize fs-5 fw-bold">full name: <span class=" text-secondary">${data.firstName}+${data.maidenName}</span> </p>
+        <p class="text-capitalize fs-5 fw-bold">address:  <span class=" text-secondary"> ${data.address.address}-${data.address.city}</span> </p>
+        <p class="text-capitalize fs-5 fw-bold">birth date: <span class=" text-secondary">  ${data.birthDate}</span></p>
+        <p class="text-capitalize fs-5 fw-bold">phone: <span class=" text-secondary">  ${data.phone}</span></p>
+        <p class="text-capitalize fs-5 fw-bold">email: <span class=" text-secondary"> ${data.email}</span> </p>
+        <p class="text-capitalize fs-5 fw-bold">univercity: <span class=" text-secondary"> ${data.university}</span></p>
+        <p class="text-capitalize fs-5 fw-bold">gender: <span class=" text-secondary"> ${data.gender}</span></p>
+        <p class="text-capitalize fs-5 fw-bold">company: <span class=" text-secondary"> ${data.company.name}--${data.company.department}--${data.company.title}</span></p>
+
+
 
     </div>
 </div>
